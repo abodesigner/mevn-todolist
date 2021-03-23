daddbcefda889eb13c15d7c03078675809d2f61c
abodesigner <mohammed.abosalim@gmail.com>
2021-03-23 13:02:39 +0200

    add README.md file

---
+++ README.md
@@ -0,0 +1,7 @@
+# How to make changes to heroku
+
+# first build the client side
+
+# then git commit
+
+# lastly git push heroku master
--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'production') {
 
     // Handle SPA
     app.get("/.*/", (req, res) => {
-        res.sendFile(__dirname + "/public/indexedDB.html")
+        res.sendFile(__dirname + "/public/index.html")
     })
 }
 
